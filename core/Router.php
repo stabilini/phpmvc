e306ccd4198e69e87ec135b30cdfbd97474307ab
Nicolas Stabilini <stabilini@gmail.com>
2024-01-16 00:58:26 -0300

    Working on requests and auth

--- core/Router.php
+++ core/Router.php
@@ -5,11 +5,13 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
-    public function __construct(\app\core\Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -17,16 +19,56 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
     public function resolve()
     {
-        
         $path = $this->request->getPath();
-        $method = $this->request->getMethod();
+        $method = $this->request->method();
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
-            echo 'Nothing found';
-            exit;
+            $this->response->setStatusCode(404);
+            return $this->renderView("_404");
         }
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->renderView($callback);
+        }
+        if (is_array($callback)) {
+            $callback[0] = new $callback[0];
+        }
+        return call_user_func($callback, $this->request);
+    }
+
+    public function renderView($view, $params = [])
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view, $params);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    public function renderContent($viewContent)
+    {
+        $layoutContent = $this->layoutContent();
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view, $params)
+    {
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,18 +1,20 @@
 <?php
 
 require_once __DIR__."/../vendor/autoload.php";
-use app\core\Application;
 
-$app = new Application();
+use \app\controllers\AuthController;
+use \app\controllers\SiteController;
+use \app\core\Application;
 
-$app->router->get('/', function()
-{
-    return 'Hello World';
-});
+$app = new Application(dirname(__DIR__));
 
-$app->router->get('/contact', function()
-{
-    return 'Contact';
-});
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [SiteController::class, 'login']);
+$app->router->post('/login', [SiteController::class, 'login']);
+$app->router->get('/register', [SiteController::class, 'register']);
+$app->router->post('/register', [SiteController::class, 'register']);
 
 $app->run();
