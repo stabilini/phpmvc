2a9b907646c1d7af73681f5b1e57e12a26deab85
Nicolas Stabilini <stabilini@gmail.com>
2024-01-18 01:12:38 -0300

    Starting migrations and database connection

---
+++ migrations/m0002_something.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0002_something
+{
+    public function up()
+    {
+        echo "Applying migration".PHP_EOL;
+    }
+
+    public function down()
+    {
+        echo "Down migration".PHP_EOL;
+    }
+}
