7657816c52efd48d12d5f4a52481903a22964db4
Nicolas Stabilini <stabilini@gmail.com>
2024-01-18 21:23:30 -0300

    Flash messages implemented

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,7 +3,8 @@
 namespace app\controllers;
 use app\core\Controller;
 use app\core\Request;
-use app\models\RegisterModel;
+use app\models\User;
+use app\core\Application;
 
 class AuthController extends Controller
 {
@@ -15,22 +16,24 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $registerModel = new RegisterModel();
+        $user = new User();
         if ($request->isPost()){
             
-            $registerModel->loadData($request->getBody());
+            $user->loadData($request->getBody());
 
-            if ($registerModel->validate() && $registerModel->register()) {
-                return 'Success';
+            if ($user->validate() && $user->save()) {
+                Application::$app->session->setFlash('success', 'Thanks for registering');
+                Application::$app->response->redirect('/');
+                exit;
             }
             
             return $this->render("register", [
-                'model' => $registerModel
+                'model' => $user
             ]);
         }
         $this->setLayout("auth");
         return $this->render("register", [
-            'model' => $registerModel
+            'model' => $user
         ]);
     }
 }
